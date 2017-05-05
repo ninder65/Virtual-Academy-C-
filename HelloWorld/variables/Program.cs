dcca6bda20b03e527286d84da224711ce00ee5d1
ninder65 <ninder65@gmail.com>
2017-05-05 05:10:39 -0400

    #merge#3

---
+++ HelloWorld/variables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int x;
+                int y;
+                x = 7;
+                y = x + 3;
+                Console.WriteLine(y);
+
+                string myFirstName, myLastName;
+                Console.WriteLine("What is your firstname?");
+                Console.Write("Type your firstname:");
+                myFirstName = Console.ReadLine();
+
+                Console.WriteLine("What is your Lastname?");
+                Console.Write("Type your Lastname:");
+                myLastName = Console.ReadLine();
+
+                Console.WriteLine("Hello," + myFirstName + " " + myLastName);
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+
